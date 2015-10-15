dbdf29d6c15a8a3ce232c4aa3dacabb799634262
Shane Kelly <shanekweb@icloud.com>
2015-10-15 15:39:53 +0100

    added search query to model

--- app/models/post.rb
+++ app/models/post.rb
@@ -4,12 +4,12 @@ attr_accessor :post_subject
 	def self.search(search)
 		if search.present?
 			rank = <<-RANK
-				ts_rank(to_tsvector(name), plainto_tsquery(#{sanitize(search)})) + 
-				ts_rank(to_tsvector(subject), plainto_tsquery(#{sanitize(search)})) +
-				ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(search)}))
+				ts_rank(to_tsvector(description), plainto_tsquery(#{sanitize(search)})) + 
+				ts_rank(to_tsvector(keyword), plainto_tsquery(#{sanitize(search)})) +
+				ts_rank(to_tsvector(productcode), plainto_tsquery(#{sanitize(search)}))
 			RANK
 			#rank above ranks items on relevence
-			where("name @@ ? OR subject @@ ? OR title @@ ?", search, search, search).order("#{rank} desc")
+			where("description @@ ? OR keyword @@ ? OR productcode @@ ?", search, search, search).order("#{rank} desc")
 			# to query words not directly beside each other in query up above only for pg
 			#where("name ILIKE ? OR subject ILIKE ?", "%#{search}%", "%#{search}%")
 		else
