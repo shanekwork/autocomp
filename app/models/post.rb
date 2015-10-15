a9f7e2a9e399539dca909e40f4b8c625d392d41c
Shane Kelly <shanekweb@icloud.com>
2015-10-15 17:14:01 +0100

    query

--- app/models/post.rb
+++ app/models/post.rb
@@ -9,9 +9,9 @@ attr_accessor :post_subject
 				ts_rank(to_tsvector(title), plainto_tsquery(#{sanitize(search)}))
 			RANK
 			#rank above ranks items on relevence
-			where("name @@ ? OR subject @@ ? OR title @@ ?", search, search, search).order("#{rank} desc")
+			#where("name @@ ? OR subject @@ ? OR title @@ ?", search, search, search).order("#{rank} desc")
 			# to query words not directly beside each other in query up above only for pg
-			#where("name ILIKE ? OR subject ILIKE ?", "%#{search}%", "%#{search}%")
+			where("name ILIKE ? OR subject ILIKE ?", "%#{search}%", "%#{search}%")
 		else
 			all
 		end
