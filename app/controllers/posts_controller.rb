afc3be5bd40ed25bfdad323a4f5f807cf6a9e1d5
Shane Kelly <shanekweb@icloud.com>
2015-10-15 17:50:50 +0100

    full pg search

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,7 @@
 class PostsController < ApplicationController
   before_action :set_post, only: [:show, :edit, :update, :destroy]
-  autocomplete :post, :subject
+  
+  autocomplete :post, :subject, :full => true
 
   # GET /posts
   # GET /posts.json
