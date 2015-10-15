f6a1142519d80b0090b7f73711a80532098789e0
Shane Kelly <shanekweb@icloud.com>
2015-10-15 15:35:48 +0100

    added protected to contr, added new fields to _form, added to index posts

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -70,6 +70,6 @@ class PostsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def post_params
-      params.require(:post).permit(:name, :title, :subject)
+      params.require(:post).permit(:name, :title, :subject, :itemcode, :productcode, :description, :keyword, :url, :image)
     end
 end
